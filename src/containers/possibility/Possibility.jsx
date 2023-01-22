c1232b57cd1c024989786d9f56f6ddbab412191e
AhmadSaleh99 <saleehsale816@gmail.com>
2023-01-22 17:02:15 +0530

    update

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./possibility.css";
+
+import possibilityImage from "../../assets/possibility.png";
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibilityImage" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradiant__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
